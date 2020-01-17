0b320179a3acb9ab8c61dc0c59b64c9f6e58b9a8
zanstaszek9 <zanstaszek9@gmail.com>
2020-01-17 02:38:10 +0000

    ServerInitializer created and configured for beginning

--- src/main/java/stanislaw/appdemo/AppdemoApplication.java
+++ src/main/java/stanislaw/appdemo/AppdemoApplication.java
@@ -2,8 +2,10 @@ package stanislaw.appdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan	// Allows to scan all classes to look for different declarations
 public class AppdemoApplication {
 
 	public static void main(String[] args) {
