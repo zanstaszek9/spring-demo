2d2d80c3111296962fcf7d7c678e4395f997d5d4
zanstaszek9 <zanstaszek9@gmail.com>
2020-01-17 03:03:40 +0000

    Added application properties with URL routing

--- src/main/java/stanislaw/appdemo/configuration/WebMvcConfig.java
+++ src/main/java/stanislaw/appdemo/configuration/WebMvcConfig.java
@@ -1,4 +1,16 @@
 package stanislaw.appdemo.configuration;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public BCryptPasswordEncoder pwdEncrypt(){
+        BCryptPasswordEncoder bcp = new BCryptPasswordEncoder();
+        return bcp;
+    }
 }
