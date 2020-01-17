ee4c068fd6d1bbdda73bd761d6b41744fc6b64c4
zanstaszek9 <zanstaszek9@gmail.com>
2020-01-17 02:06:37 +0000

    Initial commit

---
+++ src/test/java/stanislaw/appdemo/AppdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package stanislaw.appdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
