0350d860ea4834c5c00851d7096a8df996de74e2
StanislawZan <zanstaszek9@gmail.com>
2020-01-21 12:44:24 +0000

    Added Hibernate to the Role entity

--- src/main/java/stanislaw/appdemo/user/Role.java
+++ src/main/java/stanislaw/appdemo/user/Role.java
@@ -1,7 +1,17 @@
 package stanislaw.appdemo.user;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
     private int id;
+
+    @Column(name = "role")
     private String role;
 
     public int getId() {
