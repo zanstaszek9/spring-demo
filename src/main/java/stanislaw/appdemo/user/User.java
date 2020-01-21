8a72073a4c8b693dfe6af5a19f44c4cf7d0d3316
StanislawZan <zanstaszek9@gmail.com>
2020-01-21 12:37:22 +0000

    Created User Entity with Hibernate addnotations

---
+++ src/main/java/stanislaw/appdemo/user/User.java
@@ -0,0 +1,109 @@
+package stanislaw.appdemo.user;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "email")
+    @NotNull
+    private String email;
+
+
+    @Column(name = "password")
+    @NotNull
+    private String password;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Column(name = "last_name")
+    @NotNull
+    private String lastname;
+
+    @Column(name = "active")
+    @NotNull
+    private int active;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    @Transient
+    private String operation;
+
+    // Setters and Getters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+}
